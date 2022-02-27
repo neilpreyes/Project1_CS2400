c7b3c8b1e5486de460b0484d17482b807eabacb4
KenzoKhowdee <98785497+KenzoKhowdee@users.noreply.github.com>
2022-02-27 15:01:21 -0800

    Update ArrayBagTest.java

--- Project_1/ArrayBagTest.java
+++ Project_1/ArrayBagTest.java
@@ -1,5 +1,15 @@
 package CS_2400.Project_1;
 
 public class ArrayBagTest {
-    
+    BagInterface<String> bagA = new ResizeableArrayBag<>();
+        BagInterface<String> bagB = new ResizeableArrayBag<>();
+
+        bagA.add("Hi");
+        bagA.add("World");
+        bagB.add("Hello");
+        bagB.add("World");
+
+        System.out.println(Arrays.toString(bagA.union(bagB).toArray()));
+        System.out.println(Arrays.toString(bagA.intersection(bagB).toArray()));
+        System.out.println(Arrays.toString(bagA.difference(bagB).toArray()));
 }
