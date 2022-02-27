3a5d144eea556e94443e80bcbc88be887ca41c1d
KenzoKhowdee <98785497+KenzoKhowdee@users.noreply.github.com>
2022-02-27 15:05:19 -0800

    Update LinkedBagTest.java

--- Project_1/LinkedBagTest.java
+++ Project_1/LinkedBagTest.java
@@ -1,5 +1,19 @@
 package CS_2400.Project_1;
 
+import java.util.Arrays;
+
 public class LinkedBagTest {
-    
+    public static void main(String[] args){
+        BagInterface<String> bagA = new LinkedBag<>();
+        BagInterface<String> bagB = new LinkedBag<>();
+
+        bagA.add("Hi");
+        bagA.add("World");
+        bagB.add("Hello");
+        bagB.add("World");
+
+        System.out.println(Arrays.toString(bagA.union(bagB).toArray()));
+        System.out.println(Arrays.toString(bagA.intersection(bagB).toArray()));
+        System.out.println(Arrays.toString(bagA.difference(bagB).toArray()));
+    }
 }
